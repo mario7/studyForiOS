0c5b150e4451b5d89057f56fe2ba01ac32610119
snowman <from21cjp@yahoo.co.jp>
2021-01-17 20:12:01 +0900

    fix keychain access

--- KeychainSample/KeychainSample/KeychainAccess.swift
+++ KeychainSample/KeychainSample/KeychainAccess.swift
@@ -29,10 +29,12 @@ class KeychainAccess {
         let matchingStatus = SecItemCopyMatching(dic as CFDictionary, nil)
         switch matchingStatus {
         // 保存
-        case errSecItemNotFound: itemAddStatus = SecItemAdd(dic as CFDictionary, nil)
+        case errSecItemNotFound:
+            itemAddStatus = SecItemAdd(dic as CFDictionary, nil)
         // 更新
-        case errSecItemNotFound: itemAddStatus = SecItemUpdate(dic as CFDictionary,
-                                                               [kSecValueData as String: data] as CFDictionary)
+        case errSecItemNotFound:
+            itemAddStatus = SecItemUpdate(dic as CFDictionary,
+                                          [kSecValueData as String: data] as CFDictionary)
         default:
             debugPrint("キーチェーン保存失敗(\(matchingStatus)")
             return
@@ -48,21 +50,20 @@ class KeychainAccess {
                                   kSecAttrGeneric as String: key,
                                   kSecReturnData as String: kCFBooleanTrue ?? ""]
 
-        var data: AnyObject? = nil
-        let matchingStatus = withUnsafeMutablePointer(to: &data){
+        var dataTypeRef: AnyObject? = nil
+        let matchingStatus = withUnsafeMutablePointer(to: &dataTypeRef) {
             SecItemCopyMatching(dic as CFDictionary, UnsafeMutablePointer($0))
         }
-        guard let getData = data as? Data else {
-            debugPrint("キーチェーン取得失敗(Data not founde)")
-            return nil
-        }
-        var result: String? = nil
-        if matchingStatus == errSecSuccess {
-            result = String(data: getData, encoding: .utf8)
-            debugPrint("キーチェーン取得結果: \(result ?? "")")
+//      let matchingStatus: OSStatus = SecItemCopyMatching(dic as CFDictionary, &dataTypeRef)
+        
+        if matchingStatus == errSecSuccess,
+           let getData = dataTypeRef as? Data {
+                let result = String(data: getData, encoding: .utf8)
+                debugPrint("キーチェーン取得結果: \(result ?? "")")
+                return result
         } else {
             debugPrint("キーチェーン取得失敗")
         }
-        return result
+        return nil
     }
 }
